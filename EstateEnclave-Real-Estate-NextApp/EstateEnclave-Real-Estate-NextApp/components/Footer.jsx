01fbb4abcc0e7ec09515af4a93bf63cd8cfa5937
Sanskruti <sanskrutimane23@gmail.com>
2023-10-05 14:24:09 +0530

    Delete EstateEnclave-Real-Estate-NextApp/EstateEnclave-Real-Estate-NextApp directory

--- EstateEnclave-Real-Estate-NextApp/EstateEnclave-Real-Estate-NextApp/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import {Box} from '@chakra-ui/react'
-
-const Footer = () => (
-    <Box textAlign="center" p="5" color="gray.600" borderTop="gray.100">
-        2021 RealAve, Inc.
-    </Box>
-)
-
-export default Footer;
